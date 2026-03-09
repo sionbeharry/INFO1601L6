75432b2f6353be1bab5399d145eff5423cc906ba
sionbeharry <beharrysion@gmail.com>
2026-03-08 21:59:52 -0400

    e

--- public/script.js
+++ public/script.js
@@ -1,77 +1,36 @@
-function e(ele) {
-	ele.innerHTML = "e"
-	console.log("we")
+function drawTable(records){
+    let result = document.querySelector('#result');
+    //add html code inside of result
+    let html = '';// create html string
+    for(let record of records){
+        //build html string
+        html += `<tr>
+          <td>${record.id}</td>
+          <td>${record.name}</td>
+          <td>${record.brewery_type}</td>
+          <td>${record.website_url}</td>    
+        </tr>`;
+    }
+    result.innerHTML = html;//add html string to DOM
 }
 
-//get client data using navigator
-console.log(window.navigator.cookieEnabled);
-console.log(window.navigator.onLine);
-console.log(navigator.appVersion);
-console.log(navigator.userAgent)
-console.log(navigator.platform);
+//testing the function
+drawTable([{
+   id: 1,
+   name: "bob's",
+   brewery_type: "fire",
+   website_url:"https://bobs.com"
+}]);
 
-//get window metadata using window
-console.log(window.location.href);//get full url
-console.log(window.location.protocol);
-console.log(window.location.hostname);
 
-function redirect(url){
-  window.location.assign(url);//redirects the page to another url
+//function MUST be declared async
+async function getData(url){
+   try{
+     let response = await fetch(url);//1. Send http request and get response
+     let result = await response.json();//2. Get data from response
+     drawTable(result);// 3. Do something with the data
+  }catch(e){
+      console.log(e);//catch and log any errors
+  }
 }
-
-window.onload = function(event){
-   console.log("Page has loaded");
-   //do other javascript stuff here
-}
-//redirect("https:www.youtube.com")
-const result = document.querySelector('#result');
-result.innerHTML = '<h2>My Span</h2>';
-result.style.color = 'blue';
-
-
-  function myFun(){
-    alert("hello");  
-  }
-
-  function myFun2(){
-    console.log("myFun2 called");
-  }
-
- //receives the event parameter from addEventListener high order function
-  function logEventType(event){
-    console.log(event.type);
-  }
-
-  let myBtn = document.querySelector("#myBtn");
-
-  //attach myFun2 in addition to myFun to the click event of myBtn
-  myBtn.addEventListener("click", logEventType);
-
-  //There are other events such as mouseover and mouseout
-  
-  //Any callback passed to addeventListener receives an event object
-  myBtn.addEventListener("mouseover", logEventType);//logs 'mouseover'
-  myBtn.addEventListener("mouseout", logEventType);// logs 'mouseout'
- //these events will fire when the mouse pointer hovers/passes over the button
-
-  function loaded() {
-  	console.log('img loaded');
-  }
-
-let myobj = {name: "e"}
-let obj2 = myobj;
-
-//obj2 and myobj are the same variable
-
-obj2.name = "Shelly";
-
-console.log(myobj.name);//Shelly
-
-//if you need to create a new object and copy its values use Object.assign
-
-let obj3 = {};
-Object.assign(obj3, myobj);
-
-obj3.name = 'Smith';
-
-console.log(myobj.name, obj3.name);//Shelly, Smith
+getData("https://api.openbrewerydb.org/v1/breweries/search?query=harry")
